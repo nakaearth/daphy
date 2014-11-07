69fc72af2d4101ea48fb6c1a63c7a2ea303f70d4
nakamura shinichirou <naka5313@gmail.com>
2014-11-08 01:54:36 +0900

    [#81161902] 表示されたー

--- app/controllers/daphy/job_cards_controller.rb
+++ app/controllers/daphy/job_cards_controller.rb
@@ -3,18 +3,18 @@ module Daphy
     before_action :set_job_card, only: [:show, :edit]
 
     def index
-      @job_card = Todo.new
       @todos = current_user.my_job_cards.todos.page(1).per(20)
       @doings = current_user.my_job_cards.doings.page(1).per(20)
       @dones = current_user.my_job_cards.dones.page(1).per(20)
     end
 
     def new
-      @job_card = Todo.new
+      @job_card = JobCard.new
     end
 
     def create
-      job_card = Todo.new(todo_params)
+      job_card = JobCard.new(todo_params)
+      job_card.type = 'Todo'
       job_card.user = current_user
       job_card.group = current_user.group
       if job_card.save
@@ -37,7 +37,7 @@ module Daphy
     end
 
     def todo_params
-      params.require(:job_card).permit(:title, :description) if params[:job_card]
+      params.require(:job_card).permit(:title, :description, :point) if params[:job_card]
     end
   end
 end
