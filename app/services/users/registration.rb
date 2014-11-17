0fc7cece60b0f00a8e477547825c507af7878b28
nakamura shinichirou <naka5313@gmail.com>
2014-11-17 14:46:00 +0900

    トランザクション追加

--- app/services/users/registration.rb
+++ app/services/users/registration.rb
@@ -1,25 +1,27 @@
 module Users
   class Registration
     def regist(auth)
-      @login_user = User.find_or_create_by(email: auth[:info][:email]) do |user|
-        user.name  = auth[:info][:name]
-        user.email = auth[:info][:email]
-        user.provider = auth[:provider]
-        user.uid      = auth[:uid]
+      ActiveRecord::Base.transaction do
+        @login_user = User.find_or_create_by(email: auth[:info][:email]) do |user|
+          user.name  = auth[:info][:name]
+          user.email = auth[:info][:email]
+          user.provider = auth[:provider]
+          user.uid      = auth[:uid]
 
-        unless auth[:extra].blank?
-          user.nickname = auth[:extra][:raw_info][:nickname]
-          user.image_url  = auth[:extra][:raw_info][:image]
-        end
+          unless auth[:extra].blank?
+            user.nickname = auth[:extra][:raw_info][:nickname]
+            user.image_url  = auth[:extra][:raw_info][:image]
+          end
 
-        unless auth[:credentials].blank?
-          user.access_token = auth[:credentials][:token]
-          user.secret_token = auth[:credentials][:secret]
+          unless auth[:credentials].blank?
+            user.access_token = auth[:credentials][:token]
+            user.secret_token = auth[:credentials][:secret]
+          end
+          user.group = Group.find_or_create_by(name: user.name + ' group')
         end
-        user.group = Group.find_or_create_by(name: user.name + ' group')
+        @login_user.save!
+        @login_user
       end
-      @login_user.save!
-      @login_user
     end
   end
 end
