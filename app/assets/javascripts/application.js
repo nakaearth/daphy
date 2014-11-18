aa2f91720bbd16a4ef19f719d845647c0f82ba6c
nakamura shinichirou <naka5313@gmail.com>
2014-11-19 00:14:57 +0900

    ハイライトつけちゃった

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery-ui/draggable
 //= require jquery-ui/droppable
+//= require jquery-ui/effect.all
 //= require jquery-ui/effect-highlight
 //= require turbolinks
 //= require_tree .
