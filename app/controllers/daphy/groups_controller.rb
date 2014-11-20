c613dab0edef534a250f2751b3396f8f6e90f47b
nakamura shinichirou <naka5313@gmail.com>
2014-11-20 20:03:32 +0900

    グループ対応

--- app/controllers/daphy/groups_controller.rb
+++ app/controllers/daphy/groups_controller.rb
@@ -11,24 +11,47 @@ module Daphy
 
     def new
       @group = Group.new
+      @action = 'create'
     end
 
     def create
+      @group = Group.new(group_params)
+      @group.user = current_user
+      if @group.save
+        flash[:notice] = 'グループを作成しました'
+        redirect_to action: :index
+      else
+        render action: :new
+      end
     end
 
     def edit
+      @action = 'update'
     end
 
     def update
+      @group.user = current_user
+      if @group.save
+        flash[:notice] = '編集完了しました'
+        redirect_to action: :index
+      else
+        render action: :edit
+      end
     end
 
     def destroy
+      @group.destroy
+      redirect_to action: :index
     end
 
     private
 
     def set_group
       @group = Group.find(params[:id])
+    end
+
+    def group_params
+      params.require(:group).permit(:name) if params[:group]
     end
   end
 end
