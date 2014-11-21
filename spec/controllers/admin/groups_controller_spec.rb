260b31f29cb7f212242364a14e340f97d8285806
nakamura shinichirou <naka5313@gmail.com>
2014-11-21 16:49:50 +0900

    移した

--- spec/controllers/admin/groups_controller_spec.rb
+++ spec/controllers/admin/groups_controller_spec.rb
@@ -2,6 +2,29 @@ require 'rails_helper'
 
 module Admin
   RSpec.describe GroupsController, type: :controller do
+    render_views
 
+    let!(:group) { create(:group) }
+    let!(:user) { create(:user, group: group) }
+
+    before do
+      allow(controller).to receive(:current_user) { user }
+      allow(controller).to receive(:login?) { true }
+    end
+
+    describe "GET index" do
+      before do
+        get :index
+      end
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'render the :index template' do
+        expect(response).to render_template :index
+      end
+
+    end
   end
 end
