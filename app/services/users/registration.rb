3455967b248a4b825fdbe7fd1e355fd83ad02226
nakamura shinichirou <naka5313@gmail.com>
2014-12-02 01:27:00 +0900

    コミットためし

--- app/services/users/registration.rb
+++ app/services/users/registration.rb
@@ -7,10 +7,9 @@ module Users
           user.email = auth[:info][:email]
           user.provider = auth[:provider]
           user.uid      = auth[:uid]
-
           user.nickname = auth[:info][:nickname]
           user.image_url  = auth[:info][:image]
-
+          p auth[:info]
           unless auth[:credentials].blank?
             user.access_token = auth[:credentials][:token]
             user.secret_token = auth[:credentials][:secret]
