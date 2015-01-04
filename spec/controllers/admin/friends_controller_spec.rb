60c566148c2fed97fbd0f8ce7a3f3fcb5b239fbd
nakamura shinichirou <naka5313@gmail.com>
2015-01-04 23:50:35 +0900

    [#83110262]テストケース追加

--- spec/controllers/admin/friends_controller_spec.rb
+++ spec/controllers/admin/friends_controller_spec.rb
@@ -92,8 +92,8 @@ module Admin
 
     describe "GET show" do
       it "returns http success" do
-        # get :show
-        # expect(response).to have_http_status(:success)
+        get :show, id: user.friend.id
+        expect(response).to have_http_status(:success)
       end
     end
 
