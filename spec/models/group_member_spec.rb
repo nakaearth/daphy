bba27b0e40496f055c92d18704ea930451012663
nakamura shinichirou <naka5313@gmail.com>
2015-01-09 00:37:19 +0900

    [#83110262]メソッドのテスト作成はじめ

--- spec/models/group_member_spec.rb
+++ spec/models/group_member_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe GroupMember, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'not_group_member' do
+    # 引数で渡されたユーザの一覧からグループメンバー分を覗いたmono
+  end
 end
