3f5b6f37fe87767db66b4b9e90d720b0b3c8f743
nakamura shinichirou <naka5313@gmail.com>
2015-02-08 22:43:52 +0900

    まだうまくいかず

--- fluent/test/parser_test.rb
+++ fluent/test/parser_test.rb
@@ -3,13 +3,10 @@ $:.unshift(File.dirname(__FILE__))
 require 'fluent/configurable'
 require 'fluent/config'
 require 'fluent/parser'
-require 'time'
 
 # log = 'Jul 16 00:45:47 BJA login[58879]: USER_PROCESS: 58879 ttys002'
- log = "I, [2015-01-28T23:19:07.093509 #36820]  INFO -- :  'controller': 'job_cards', 'action': 'index', 'execute_time': '2015-01-28 23:19:07 +0900'"
+log = 'Jul 16 00:45:47 BJA login[58879]: USER_PROCESS: 58879 ttys002'
 
-regexp = /^I, \[(?<time>[^\.]+).+\]  INFO -- : 'controller': (?<conotroller>[^ ]+) .+ 'action': (?<benchmark>[^m]+)ms.*$/
-regexp = Regexp.new(regexp) 
+regexp = /^(?<date>\w{3} \d\d (\d{2}:){2}\d{2}) (?<host>\w+) (?<role>\w+)\[(?<pid>\d+)\]: (?<event>\w+): (?<info>\d+) (?<tty>.+)$/
 parser = Fluent::TextParser::RegexpParser.new(regexp)
-parser.time_format = '%b %d %T'
 p parser.parse(log)
