0e45fe73cd4b2a867e90e666d07424163ca36864
nakamura shinichirou <naka5313@gmail.com>
2015-03-06 00:49:21 +0900

    rakeについか

--- lib/tasks/jobs/notification.rb
+++ lib/tasks/jobs/notification.rb
@@ -4,8 +4,11 @@ module Job
       JobCard.find_each do |job|
         if job.schedule_day_overdue?
           # TODO: ここに処理を書く
+          # Notifications.create(message: '期限が過ぎております')
         end
       end
+    rescue
+      logger.error '処理中にエラーが発生しました。'
     end
   end
 end
