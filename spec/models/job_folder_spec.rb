c7d57098181f4951655991d7b1704896ea6f05fb
nakamura shinichirou <naka5313@gmail.com>
2015-03-21 23:59:25 +0900

    テスト

--- spec/models/job_folder_spec.rb
+++ spec/models/job_folder_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe JobFolder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validate' do
+    describe 'name' do
+      context 'name length > 50' do
+        it { expect{JobFolder.new.save!(name: "aaaaaaaaaa" * 6)}.to raise_error }
+      end
+    end
+  end
 end
