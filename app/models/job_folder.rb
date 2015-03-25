de452e3e35c3ecdd49abe169022e51cda5b64e20
nakamura shinichirou <naka5313@gmail.com>
2015-03-26 01:55:04 +0900

    アソシエーション修正

--- app/models/job_folder.rb
+++ app/models/job_folder.rb
@@ -1,7 +1,7 @@
 class JobFolder < ActiveRecord::Base
   belongs_to :group
   has_many :job_cards
-  accepts_nested_attributes_for :job_cardsw
+  accepts_nested_attributes_for :job_cards
 
   validates :name, presence: true, length: { maximum: 50 }
 
