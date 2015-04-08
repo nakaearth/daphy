6deb06c38e6a4f15b8f60726aa0f1f875676b6e2
nakaearth <naka5313@gmail.com>
2015-04-08 10:57:14 +0900

    実験2

--- all_test.rb
+++ all_test.rb
@@ -2,7 +2,6 @@ ENV['RAILS_ENV'] ||= 'test'
 ENV['TEST_QUEUE_STATS'] ||= File.expand_path('../tmp/.test_queue_stats', __FILE__)
 
 require File.expand_path('../config/environment', __FILE__)
-
 require 'test_queue'
 require 'test_queue/runner/rspec'
 
