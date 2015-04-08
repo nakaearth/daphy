8cdfbce2a56632b6e08e4b85f8f1c8bc54fac9a1
nakaearth <nakamura.shinichiro@moneyforward.co.jp>
2015-04-08 10:27:33 +0900

    戻し

--- all_test.rb
+++ all_test.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 ENV['TEST_QUEUE_STATS'] ||= File.expand_path('../tmp/.test_queue_stats', __FILE__)
 
 require File.expand_path('../config/environment', __FILE__)
+
 require 'test_queue'
 require 'test_queue/runner/rspec'
 
