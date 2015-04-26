225077160162613f4e80e2cae60dc7816e6cfaf5
nakamura shinichirou <naka5313@gmail.com>
2015-04-27 07:01:13 +0900

    groupのテスト追加

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 describe Group, type: :model do
+  describe '幾つかのテーブルと関連を持っている' do
+    context 'have a relation to user class' do
+      it { expect have_many(:job_forlders) }
+    end
+  end
+
   describe '入力チェックをする' do
     context 'nameは必須' do
       it { expect validate_presence_of(:name) }
